2a14734e13314e51887c10d90695d5feca440b87
iManar <manar.magdy@booking.com>
2021-03-31 18:22:27 +0200

    [S-03] Creatued featured slider view

---
+++ Touchdown/Extension/CodableBundleExtension.swift
@@ -0,0 +1,8 @@
+//
+//  CodableBundleExtension.swift
+//  Touchdown
+//
+//  Created by Manar Magdy on 31/03/2021.
+//
+
+import Foundation
---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Manar Magdy on 31/03/2021.
+//
+
+import Foundation
