853b581aaa844e2230a1d99793dab85da40b8260
iManar <manar.magdy@booking.com>
2021-03-31 16:35:22 +0200

    [S-03] Added app resources and categorization groups

--- Touchdown/App/TouchdownApp.swift
+++ Touchdown/App/TouchdownApp.swift
@@ -2,7 +2,7 @@
 //  TouchdownApp.swift
 //  Touchdown
 //
-//  Created by Manar Magdy on 31/03/2021.
+//  Created by Manar Magdy ♛ on 31/03/2021.
 //
 
 import SwiftUI
---
+++ Touchdown/Utility/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  Touchdown
+//
+//  Created by Manar Magdy ♛ on 31/03/2021.
+//
+
+import SwiftUI
+
+// DATA
+
+
+// COLOR
+
+let colorBackground = Color("ColorBackground")
+let colorGray = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
