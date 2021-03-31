cdc85811048b283e0e24a7367ef2be26c4b1fb63
iManar <manar.magdy@booking.com>
2021-03-31 17:19:04 +0200

    [S-03] Created the animated navigation bar

---
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Manar Magdy on 31/03/2021.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+    }
+}
